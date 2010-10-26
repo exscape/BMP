1ab9a840272199eac1f188a1772e7171e0fc072e
Thomas Backman <serenity@exscape.org>
2010-10-26 18:12:36 +0200

    Minor tweaks

--- bmp.py
+++ bmp.py
@@ -24,17 +24,15 @@ bitmap_offset = bmp_header[5]
 prevpos = f.tell()
 f.seek(0, 2) # Seek to end of file, to check its size
 if f.tell() != file_size:
-	die("Malformed header; file size in header doesn't match actual file size; {0} vs {1}".format(f.tell(), file_size))
+	die("Malformed header; file size in header doesn't match actual file size; {0} vs {1} bytes".format(f.tell(), file_size))
 f.seek(prevpos, 0)
-
-if DEBUG: print "BMP header good; data offset = {0}".format(bitmap_offset)
 
 dib_header_len = unpack('I', f.read(4))[0]
 
 if dib_header_len == 64:
 	die("V2 DIB-header BMPs not supported")
 elif dib_header_len != 40:
-	warn("V1/V4/V5 header. Stuff may break... but should work.")
+	warn("V1/V4/V5 DIB header. Stuff may break... but should work.")
 
 f.seek(-4, 1) # Re-read the header
 dib_header = "" # Lexical scoping
@@ -42,24 +40,27 @@ dib_header = "" # Lexical scoping
 if dib_header_len == 12:
 	# OS/2 V1 header
 	dib_header = unpack('I4H', f.read(12))
+	if DEBUG: print 'OS/2 V1 DIB header'
 elif dib_header_len in (40,108,124):
 	# Windows V3/V4/V5 header - only the necessary parts are read
 	dib_header = unpack('I2i2H', f.read(16))
+	if DEBUG: print 'V3/V4/V5 DIB header'
 else:
 	die("Unsupported/corrupt DIB header")
 
-print 'DIB header (relevant parts):', dib_header
+if DEBUG: print 'DIB header (relevant parts):', dib_header
 
 (dib_header_len,width,height,color_planes,bpp) = dib_header
 
 if color_planes != 1:
 	die("Invalid/corrupt DIB header; # of color planes must be 1")
+
 if bpp != 24:
 	die("Only 24 bits per pixel is supported at the moment; this image appears to be {0}".format(bpp))
 
 print 'Image is {0}x{1} @ {2} bpp'.format(width, height, bpp)
 
-# If the header was a V4/V5 one we need to skip ahead to get to the actual data
+# We need to skip te rest of the header, if any, to get to the actual data
 f.seek(bitmap_offset) 
 
 bitmap_data = f.read()
