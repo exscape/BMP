2677ab8598a52a0a224b3f2b285cd20a34fc4568
Thomas Backman <serenity@exscape.org>
2010-10-26 20:37:46 +0200

    Minor cleanups

--- bmp.py
+++ bmp.py
@@ -1,7 +1,5 @@
 DEBUG=True
-
-import sys
-from struct import pack,unpack
+import sys, struct
 
 def die(str="Unspecified error"):
 	sys.stderr.write("Fatal error: {0}\n".format(str))
@@ -9,15 +7,15 @@ def die(str="Unspecified error"):
 
 def warn(str="FIXME"):
 	sys.stderr.write("Warning: {0}\n".format(str))
+	sys.stderr.flush()
 
-filename = ""
 if len(sys.argv) == 1:
 	filename = "test.bmp"
 else:
 	filename = sys.argv[1]
 
 f = open(filename, 'r')
-bmp_header = unpack('<2bIHHI', f.read(14))
+bmp_header = struct.unpack('<2bIHHI', f.read(14))
 magic = str(chr(bmp_header[0])) + chr(bmp_header[1])
 if DEBUG: print 'BMP header:', bmp_header
 
@@ -33,7 +31,7 @@ if f.tell() != file_size:
 	die("Malformed header; file size in header doesn't match actual file size; {0} (actual) vs {1} (header) bytes".format(f.tell(), file_size))
 f.seek(prevpos, 0)
 
-dib_header_len = unpack('I', f.read(4))[0]
+dib_header_len = struct.unpack('I', f.read(4))[0]
 
 if dib_header_len == 64:
 	die("V2 DIB-header BMPs not supported")
@@ -44,16 +42,16 @@ f.seek(-4, 1) # Re-read the header
 
 if dib_header_len == 12:
 	# OS/2 V1 header
-	dib_header = unpack('I4H', f.read(12))
+	dib_header = struct.unpack('I4H', f.read(12))
 	if DEBUG: print 'OS/2 V1 DIB header'
 elif dib_header_len in (40,108,124):
 	# Windows V3/V4/V5 header - only the necessary parts are read
-	dib_header = unpack('I2i2H', f.read(16))
+	dib_header = struct.unpack('I2i2H', f.read(16))
 	if DEBUG: print 'V3/V4/V5 DIB header'
 else:
 	die("Unsupported/corrupt DIB header")
 
-if DEBUG: print 'DIB header (relevant parts):', dib_header
+if DEBUG: print 'DIB header:', dib_header
 
 (dib_header_len,width,height,color_planes,bpp) = dib_header
 
@@ -61,7 +59,7 @@ if color_planes != 1:
 	die("Invalid/corrupt DIB header; # of color planes must be 1")
 
 if bpp != 24:
-	die("Only 24 bits per pixel is supported at the moment; this image appears to be {0}".format(bpp))
+	die("Only 24 bits per pixel is supported at the moment; this image appears to have {0}".format(bpp))
 
 print 'Image is {0}x{1} @ {2} bpp'.format(width, height, bpp)
 
