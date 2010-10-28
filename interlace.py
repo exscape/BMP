b8cefe7552baf6adbd7708e07858908c836ca487
Thomas Backman <serenity@exscape.org>
2010-10-28 10:05:19 +0200

    Add interlace.py, a simple script that uses the class to change every 2 rows to a darker color

---
+++ interlace.py
@@ -0,0 +1,36 @@
+import sys
+from bmp import BMP
+from cStringIO import StringIO
+
+if len(sys.argv) == 1:
+	filename = "test.bmp"
+else:
+	filename = sys.argv[1]
+
+b = BMP(filename)
+
+print 'Image is {0}x{1} @ {2} bpp'.format(b.width, b.height, b.bpp)
+
+print 'Working...',
+sys.stdout.flush()
+
+mod_bitmap = ""
+
+# Let's pretend it's a file to make things easy
+f = StringIO(b.bitmap_data)
+for row_num in xrange(0, b.height):
+	if row_num & 1 == 0:
+		# Simply copy the data from even rows...
+		mod_bitmap += f.read((b.width * 3) + b.padding_size)
+	else:
+		# ... and divide all pixel values by two on odd rows
+		data = f.read((b.width * 3) + b.padding_size)
+		mod_bitmap += "".join(map (lambda x: chr(ord(x)/2), data))
+
+# ... and then save the results, with unmodified headers, since the size, bpp etc. are all the same
+out = open('out.bmp', 'w')
+out.write(b.all_headers)
+out.write(mod_bitmap)
+out.close()
+
+print 'done'
